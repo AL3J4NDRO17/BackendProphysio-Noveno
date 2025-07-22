88e8d8a1b5cc77f0c33bf2349a2baa4c120c5796
AlejandroEspinosa <116526021+AL3J4NDRO17@users.noreply.github.com>
2025-07-22 01:19:49 -0600

    Update corsSetup.js

--- src/setup/corsSetup.js
+++ src/setup/corsSetup.js
@@ -1,7 +1,7 @@
 const { CLIENT_URL } = require("../config/index");
 console.log("CLIENT_URL", CLIENT_URL);
 const corsOptions = {
-    origin: [CLIENT_URL,"http://10.0.2.2:3000"],
+    origin: [CLIENT_URL,"http://10.0.2.2:3000",'http://localhost:3000'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "X-CSRF-Token"], //Permitir el header CSRF en la solicitud
